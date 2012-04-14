606f3ae69f7968e32b5ee0924a07a27dc732a503
Chiku <chirantan.mitra@gmail.com>
2012-04-14 12:16:48 +0530

    Add views and routers

--- app.js
+++ app.js
@@ -1,35 +1,80 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
-  , routes = require('./routes');
-
-var app = module.exports = express.createServer();
-
-// Configuration
+  , app = express.createServer()
+  , util = require('util')
+  , mongoose = require('mongoose')
+  , db = mongoose.connect('mongodb://localhost:27017/db');
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.use(express.logger());
+  app.use(express.errorHandler());
+  app.use(express.static(__dirname + '/public'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.set('views', __dirname + '/views'); 
+app.set('view engine', 'jade');
+
+var Product = require ('./lib/product').Product
+  , ProductStore = db.model('Product');
+
+app.get('/', function(request, response) {
+  response.redirect('/products');
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
+app.get('/products', function(request, response){
+  ProductStore.find(function(error, products){
+    response.render('products/index', {locals: {
+      products: products
+    }});
+  });
 });
 
-// Routes
+app.get('/products/:id', function(request, response){
+  ProductStore.findById(request.params.id, function(error, product){
+    response.render('products/show', {
+      locals: {
+        product: product
+      }
+    })
+  });
+});
 
-app.get('/', routes.index);
+app.get('/products/new', function(request, response) {
+  response.render('products/new', {
+    locals: {
+      product: request.body && request.body.product || new ProductStore()
+    }
+  });
+});
+
+app.post('/products', function(request, response) {
+  var product = new ProductStore(request.body.product);
+  product.save(function(){
+    response.redirect('/products/' + product._id.toHexString());
+  });
+});
+
+app.get('/products/:id/edit', function(request, response){
+  var product = ProductStore.find(request.params.id);
+  ProductStore.findById(request.params.id, function(error, product){
+    response.render('products/edit', {
+      locals: {
+        product: product
+      }
+    });    
+  });
+});
+
+app.put('/products/:id', function(request, response){
+  ProductStore.findById(request.params.id, function(error, product) {
+    product.name = request.body.product.name;
+    product.description = request.body.product.description;
+    product.price = request.body.product.price;
+    product.save(function(){
+      response.redirect('/products/' + product._id.toHexString());
+    });
+  });
+});
 
 app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
