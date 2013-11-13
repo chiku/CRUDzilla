e01f69e6637fa486519dbe51479f7bd539e0ad8b
Chirantan Mitra <chirantan.mitra@gmail.com>
2013-11-13 17:24:44 +0530

    Code reformat

--- app.js
+++ app.js
@@ -4,75 +4,76 @@ var express = require('express'),
     mongoose = require('mongoose'),
     db = mongoose.connect('mongodb://localhost:27017/db');
 
-app.configure(function(){
-  app.use(express.logger());
-  app.use(express.errorHandler());
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
+app.configure(function() {
+    app.use(express.logger());
+    app.use(express.errorHandler());
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
 });
 
-app.set('views', __dirname + '/views'); 
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.set("view options", { layout: "layout.jade" });
+app.set("view options", {
+    layout: "layout.jade"
+});
 
-var Product = require ('./lib/product').Product,
+var Product = require('./lib/product').Product,
     ProductStore = db.model('Product');
 
 app.get('/', function(request, response) {
-  response.redirect('/products');
+    response.redirect('/products');
 });
 
-app.get('/products', function(request, response){
-  ProductStore.find(function(error, products){
-    response.render('products/index', {
-      products: products
+app.get('/products', function(request, response) {
+    ProductStore.find(function(error, products) {
+        response.render('products/index', {
+            products: products
+        });
     });
-  });
 });
 
 app.get('/products/new', function(request, response) {
-  var product = (request.body && request.body.product) || new ProductStore();
-  response.render('products/new', {
-    product: product
-  });
+    var product = (request.body && request.body.product) || new ProductStore();
+    response.render('products/new', {
+        product: product
+    });
 });
 
-app.get('/products/:id/edit', function(request, response){
-  var product = ProductStore.find(request.params.id);
-  ProductStore.findById(request.params.id, function(error, product){
-    response.render('products/edit', {
-      product: product
-    });    
-  });
+app.get('/products/:id/edit', function(request, response) {
+    var product = ProductStore.find(request.params.id);
+    ProductStore.findById(request.params.id, function(error, product) {
+        response.render('products/edit', {
+            product: product
+        });
+    });
 });
 
-app.get('/products/:id', function(request, response){
-  ProductStore.findById(request.params.id, function(error, product){
-    response.render('products/show', {
-      product: product
-    })
-  });
+app.get('/products/:id', function(request, response) {
+    ProductStore.findById(request.params.id, function(error, product) {
+        response.render('products/show', {
+            product: product
+        })
+    });
 });
 
 app.post('/products', function(request, response) {
-  var product = new ProductStore(request.body.product);
-  product.save(function(){
-    response.redirect('/products/' + product._id.toHexString());
-  });
+    var product = new ProductStore(request.body.product);
+    product.save(function() {
+        response.redirect('/products/' + product._id.toHexString());
+    });
 });
 
-
-app.put('/products/:id', function(request, response){
-  ProductStore.findById(request.params.id, function(error, product) {
-    var submittedProduct = request.body.product;
-    product.name = submittedProduct.name;
-    product.description = submittedProduct.description;
-    product.price = submittedProduct.price;
-    product.save(function() {
-      response.redirect('/products/' + product._id.toHexString());
+app.put('/products/:id', function(request, response) {
+    ProductStore.findById(request.params.id, function(error, product) {
+        var submittedProduct = request.body.product;
+        product.name = submittedProduct.name;
+        product.description = submittedProduct.description;
+        product.price = submittedProduct.price;
+        product.save(function() {
+            response.redirect('/products/' + product._id.toHexString());
+        });
     });
-  });
 });
 
 app.listen(3000);
