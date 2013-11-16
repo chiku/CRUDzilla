9ff78a6a5bdd2187576bc255d7b0508fca7ebe05
Chirantan Mitra <chirantan.mitra@gmail.com>
2013-11-16 08:23:06 +0530

    Internationalized can accept tokens without a language. It returns a function that resolves to a token for a language.

---
+++ test/internationatization.test.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var should = require("should"),
+    i18l = require("../lib/internationalization").i18l;
+
+describe("Internationalization", function() {
+    describe("when referred by token and language", function() {
+        it("fetches the token for the language", function() {
+            i18l("nameMissing", "en-US").should.equal("Name can't be blank");
+        })
+    });
+
+    describe("when referred by token", function() {
+        it("returns a function that can return the tokens for a language", function(){
+            i18l("priceNotNumber")("hi").should.equal("मूल्य एक संख्या होनी चाहिए");
+        });
+    });
+});
